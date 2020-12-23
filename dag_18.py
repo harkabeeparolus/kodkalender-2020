92d7b18735b2410ae38420c34db42bd5d9576bef
Fredrik Mellström <11281108+harkabeeparolus@users.noreply.github.com>
2020-12-23 22:17:42 +0100

    Dag 18

---
+++ dag_18.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+
+"""Unga programmerare kodkalender 2020, lucka 18"""
+# https://ungaprogrammerare.se/kodkalender/lucka-18/
+
+from input_files.dag_18_input import tallista
+
+
+def is_palindrome(number: int) -> bool:
+    "Test if a number is a palindrome"
+    as_string = str(number)
+    return as_string == as_string[::-1]
+
+
+palindromes = [n for n in tallista if is_palindrome(n)]
+
+print(*palindromes, sep=", ")
+print(f"Antal palindrom: {len(palindromes)}")
+
+# 34743, 29392, 72627
+# Antal palindrom: 3
