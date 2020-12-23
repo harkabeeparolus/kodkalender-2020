c6261e542c918a037a9c0cb4863086c3ff4baf08
Fredrik Mellström <11281108+harkabeeparolus@users.noreply.github.com>
2020-12-23 18:03:55 +0100

    Dag 12

---
+++ dag_12.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python3
+
+"""Unga programmerare kodkalender 2020, lucka 12"""
+# https://ungaprogrammerare.se/kodkalender/lucka-12/
+
+from dag_12_input import tallista
+
+
+def collatz(number: int) -> int:
+    "Collatz Problem"
+    count = 0
+    while number > 1:
+        if number % 2:
+            number = number * 3 + 1  # odd
+        else:
+            number /= 2  # even
+        count += 1
+    return count
+
+
+winner = sum(collatz(n) for n in tallista)
+
+print(f"Vinstlott: {winner}")
+# Vinstlott: 1337
