d0762afc322fb41d96619a334c46b52851a2389d
Fredrik Mellström <11281108+harkabeeparolus@users.noreply.github.com>
2020-12-23 19:54:38 +0100

    Dag 14

---
+++ dag_14.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+
+"""Unga programmerare kodkalender 2020, lucka 14"""
+# https://ungaprogrammerare.se/kodkalender/lucka-14/
+
+from itertools import combinations
+
+from dag_14_input import artikelnummer
+
+numbers = (str(num) for num in artikelnummer)
+anagrams = [(i, j) for i, j in combinations(numbers, 2) if sorted(i) == sorted(j)]
+assert len(anagrams) == 1
+
+print(f"Artikelnummer: {anagrams.pop()}")
+# Artikelnummer: ('95094', '59049')
