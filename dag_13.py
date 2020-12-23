04f7b3e09aeca574f9d122830fd7924f3a598de5
Fredrik Mellström <11281108+harkabeeparolus@users.noreply.github.com>
2020-12-24 00:12:09 +0100

    itertools recipe pairwise

--- dag_13.py
+++ dag_13.py
@@ -3,16 +3,25 @@
 """Unga programmerare kodkalender 2020, lucka 13"""
 # https://ungaprogrammerare.se/kodkalender/lucka-13/
 
+import itertools
+
 from input_files.dag_13_input import tallista
+
+
+def pairwise(iterable):
+    "Collect data into pairs"
+    # s -> (s0,s1), (s1,s2), (s2, s3), ...
+    # https://docs.python.org/3/library/itertools.html#itertools-recipes
+    a, b = itertools.tee(iterable)
+    next(b, None)
+    return zip(a, b)
 
 
 def number_ok(number: int) -> bool:
     "Validate a number"
-    as_string = str(number)
-    assert len(as_string) >= 2
-    pairs = [
-        (int(as_string[i]), int(as_string[i + 1])) for i in range(len(as_string) - 1)
-    ]
+    num_str = str(number)
+    assert len(num_str) >= 2
+    pairs = [map(int, pair) for pair in pairwise(num_str)]
     return all(a == b or a < b for a, b in pairs)
 
 
--- dag_21.py
+++ dag_21.py
@@ -3,7 +3,18 @@
 """Unga programmerare kodkalender 2020, lucka 21"""
 # https://ungaprogrammerare.se/kodkalender/lucka-21/
 
+import itertools
+
 from input_files.dag_21_input import lista
+
+
+def pairwise(iterable):
+    "Collect data into pairs"
+    # s -> (s0,s1), (s1,s2), (s2, s3), ...
+    # https://docs.python.org/3/library/itertools.html#itertools-recipes
+    a, b = itertools.tee(iterable)
+    next(b, None)
+    return zip(a, b)
 
 
 def is_rainbow_number(number: int) -> bool:
@@ -12,12 +23,12 @@ def is_rainbow_number(number: int) -> bool:
 
     num_str = str(number)
 
-    palindrome = (num_str == num_str[::-1])
-    has_ones = (num_str[0] == num_str[-1] == "1")
+    palindrome = num_str == num_str[::-1]
+    has_ones = num_str[0] == num_str[-1] == "1"
 
     half_length = len(num_str) // 2
     half = num_str[:-half_length]
-    pairs = [(int(half[i]), int(half[i + 1])) for i in range(len(half) - 1)]
+    pairs = [map(int, pair) for pair in pairwise(half)]
     in_sequence = all(0 <= (b - a) <= 1 for a, b in pairs)
 
     return palindrome and has_ones and in_sequence
