6509a8d0bed910560c452762d52def1960aa364f
Fredrik Mellström <11281108+harkabeeparolus@users.noreply.github.com>
2020-12-23 17:39:36 +0100

    Dag 10

---
+++ dag_10.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+
+"""Unga programmerare kodkalender 2020, lucka 10"""
+# https://ungaprogrammerare.se/kodkalender/lucka-10/
+
+from dag_10_input import hacker_lista
+
+
+def first_plus_last(num: int) -> int:
+    "Sum first and last digits of an integer"
+    s = str(num)
+    return int(s[0]) + int(s[-1])
+
+
+total = sum(first_plus_last(num) for num in hacker_lista)
+
+print(f"Lösenord: {total}")
+# Lösenord: 7233
